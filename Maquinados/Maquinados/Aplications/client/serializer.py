ebe0fba80731b8f681996c236ff27ebc165c73a5
Julian Rico <julianricomillan@gmail.com>
2022-10-22 10:16:11 -0500

    Changes on item models

--- Maquinados/Maquinados/Aplications/client/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from Aplications.client.models import Client
-
-class ClientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        exclude = ["is_active"]
--- Maquinados/Maquinados/Aplications/client/url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ClientDetail, ClientList
-
-
-urlpatterns = [
-    path("", ClientList.as_view()),
-    path("<int:id>/", ClientDetail)
-]
