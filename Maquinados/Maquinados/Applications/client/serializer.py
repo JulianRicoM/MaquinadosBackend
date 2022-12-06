bff639e84312aa38c38a6fb410248854f157b71c
Julian Rico <julianricomillan@gmail.com>
2022-12-05 23:02:37 -0500

    Update client form

--- Maquinados/Maquinados/Applications/client/serializer.py
+++ Maquinados/Maquinados/Applications/client/serializer.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 
-from Applications.client.models import Client
+from Applications.client.models import Client, Country, City, Department
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         exclude = ["is_active"]
+    
+class CountriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = "__all__"
+
+class CitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = "__all__"
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = "__all__"
